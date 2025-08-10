efb58234dd8655c407f3d379c177e394c970e4b6
Carla Magalhaes <carlamagalhaes60@gmail.com>
2025-08-09 21:39:50 -0300

    input e botão

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,22 @@
 import './App.css'
 
 function App() {
-  
+
 
   return (
     <>
-      <div>
-
-      </div>
+      <div className='container'>
         
+        <div className='quadrado'>
+          <h1 className='titulo'>To do List</h1>
+          <div className='task-input'>
+            <input type="text" className='input' placeholder='Adicionar uma tarefa' />
+            <button className='button'>
+              Adiconar
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
